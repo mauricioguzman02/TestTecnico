f9930ff18383dfc53fc20a3e6c9a005b074b6cc6
Oscar Sierra <oscar.sierra@hubse.com>
2023-11-28 19:54:30 -0500

    upload files test tecnico

---
+++ UnitTestProject1/Registro.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class Registro
+    {
+        private IWebDriver driver;
+
+        // Elementos de la página
+        public IWebElement Registrar => driver.FindElement(By.XPath("//unauthenticated-header/div[1]/div[3]/ul[2]/li[2]/a[1]"));
+        public IWebElement FirtsName => driver.FindElement(By.Id("firstName"));
+        public IWebElement Password => driver.FindElement(By.Id("password"));
+        public IWebElement TerminosUso => driver.FindElement(By.Id("termOfUse"));
+        public IWebElement Aceptar => driver.FindElement(By.Id("privacySetting"));
+        public IWebElement Terminar => driver.FindElement(By.Id("laddaBtn"));
+        
+
+        public IWebElement Password2 => driver.FindElement(By.CssSelector("#confirmPassword"));
+        public IWebElement LastName => driver.FindElement(By.Id("lastName"));
+        public IWebElement Siguiente1 => driver.FindElement(By.XPath("//span[contains(text(),'Next: Location')]"));
+        public IWebElement Siguiente2 => driver.FindElement(By.XPath("//span[contains(text(),'Next: Devices')]"));
+        public IWebElement Siguiente3 => driver.FindElement(By.XPath("//span[contains(text(),'Next: Last Step')]"));
+        
+        public IWebElement Email => driver.FindElement(By.Id("email"));
+        public IWebElement Dia => driver.FindElement(By.XPath("//select[@id='birthDay']"));
+        public SelectElement DiaSelect =>  new SelectElement(Dia);
+
+        public IWebElement Mes => driver.FindElement(By.XPath("//select[@id='birthMonth']"));
+        public SelectElement MesSelect => new SelectElement(Mes);
+
+        public IWebElement Anio => driver.FindElement(By.XPath("//select[@id='birthYear']"));
+        public SelectElement AnioSelect => new SelectElement(Anio);
+        // Constructor
+        public Registro(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+   }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using Assert = NUnit.Framework.Assert;
+using Microsoft.Win32;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestAutomation
+    {
+        IWebDriver driver;
+        Registro registro;
+
+        [SetUp]
+        public void Inicializar()
+        {
+            // Inicializar el controlador del navegador
+            driver = new ChromeDriver();
+            registro = new Registro(driver);
+        }
+
+        [Test]
+        public void MiPruebaConPOM()
+        {
+            driver.Navigate().GoToUrl("https://utest.com/");
+            Thread.Sleep(3000);
+            registro.Registrar.Click();
+            Thread.Sleep(3000);
+            registro.FirtsName.SendKeys("Mauricio");
+            registro.LastName.SendKeys("Guzman");
+            registro.Email.SendKeys("MaoGuzman" + new Random().Next(1, 1000) + "@gmail.com");
+            registro.DiaSelect.SelectByIndex(1);
+            registro.MesSelect.SelectByIndex(1);
+            registro.AnioSelect.SelectByIndex(3);
+            Thread.Sleep(3000);
+            registro.Siguiente1.Click();
+            Thread.Sleep(3000);
+            registro.Siguiente2.Click();
+            Thread.Sleep(3000);
+            registro.Siguiente3.Click();
+            Thread.Sleep(3000);
+            registro.Password.SendKeys("Sistemas2020*");
+            Thread.Sleep(3000);
+            registro.Password2.SendKeys("Sistemas2020*");
+            registro.TerminosUso.Click();
+            registro.Aceptar.Click();
+            Thread.Sleep(3000);
+            registro.Terminar.Click();
+            Thread.Sleep(3000);
+            Assert.AreEqual(true,true);
+        }
+
+        [TearDown]
+        public void Limpiar()
+        {
+            // Cerrar el navegador al finalizar la prueba
+            driver.Quit();
+        }
+    }
+}
